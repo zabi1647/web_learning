7396c07851ab54801ff935aa815239ee77309d8e
Zohaib <101661680+zabi1647@users.noreply.github.com>
2024-03-19 04:30:09 +0500

    learning js

---
+++ cwh/script.js
@@ -0,0 +1,8 @@
+alert('hello world!');
+console.log('hello zohaib');
+var a = prompt('Enter your name');
+
+var b = confirm('Are you sure you want to leave this page?');
+if(b){
+    console.log('User wants to leave the page');
+}
